3836d202eb5c24021f613b241aee246ebd808f11
Kyle Oba <koba@mudphone.com>
2016-12-29 11:59:37 -0800

    Add through Zip Applicative exercises

---
+++ src/Seventeen.hs
@@ -0,0 +1,117 @@
+module Seventeen where
+
+import Data.List (elemIndex)
+
+-- Exercises: Lookups
+-- 1
+added :: Maybe Integer
+added = pure (+3) <*> (lookup 3 $ zip [1, 2, 3] [4, 5, 6])
+
+-- 2
+y :: Maybe Integer
+y = lookup 3 $ zip [1, 2, 3] [4, 5, 6]
+
+z :: Maybe Integer
+z = lookup 2 $ zip [1, 2, 3] [4, 5, 6]
+
+tupled :: Maybe (Integer, Integer)
+tupled = (,) <$> y <*> z
+
+-- 3
+x3 :: Maybe Int
+x3 = elemIndex 3 [1, 2, 3, 4, 5]
+
+y3 :: Maybe Int
+y3 = elemIndex 4 [1, 2, 3, 4, 5]
+
+max' :: Int -> Int -> Int
+max' = max
+
+maxed :: Maybe Int
+maxed = pure max' <*> x3 <*> y3
+
+-- 4
+xs = [1, 2, 3]
+ys = [4, 5, 6]
+
+x4 :: Maybe Integer
+x4 = lookup 3 $ zip xs ys
+
+y4 :: Maybe Integer
+y4 = lookup 2 $ zip xs ys
+
+summed :: Maybe Integer
+summed = fmap sum $ (,) <$> x4 <*> y4
+
+
+-- Exercise: Identity Instance
+newtype Identity a = Identity a
+  deriving (Eq, Ord, Show)
+
+instance Functor Identity where
+  fmap f (Identity a) = Identity (f a) 
+
+instance Applicative Identity where
+  pure = Identity
+  (<*>) (Identity f) (Identity a) = Identity (f a) 
+
+
+-- Exercise: Constant Instance
+newtype Constant a b =
+  Constant { getConstant :: a }
+  deriving (Eq, Ord, Show)
+
+instance Functor (Constant a) where
+  fmap _ (Constant a) = Constant a
+
+instance Monoid a => Applicative (Constant a) where
+  pure _ = Constant mempty
+  (<*>) (Constant a1) (Constant a2) = Constant (mappend a1 a2)
+
+
+-- Exercise: Fixer Upper
+-- 1
+fixerUpper1 = const <$> Just "Hello" <*> pure "World"
+
+-- 2
+fixerUpper2 = (,,,) <$> Just 90 <*> Just 10 <*> Just "Tierness" <*> pure [1, 2, 3]
+
+
+-- List Applicative Exercise
+data List a =
+    Nil
+  | Cons a (List a)
+  deriving (Eq, Show)
+
+instance Functor List where
+  fmap _ Nil = Nil
+  fmap f (Cons a list) = Cons (f a) (fmap f list)
+
+append :: List a -> List a -> List a
+append Nil ys = ys
+append (Cons x xs) ys = Cons x $ xs `append` ys
+
+fold :: (a -> b -> b) -> b -> List a -> b
+fold _ b Nil = b
+fold f b (Cons h t) = f h (fold f b t)
+
+concat' :: List (List a) -> List a
+concat' = fold append Nil
+
+flatMap :: (a -> List b) -> List a -> List b
+flatMap _ Nil = Nil
+flatMap f as = concat' $ fmap f as
+
+toMyList = foldr Cons Nil
+myXs = toMyList [1,2,3]
+myC = Cons
+myFlatMap = flatMap (\x -> x `myC` (9 `myC` Nil)) myXs
+
+functions = Cons (+1) (Cons (*2) Nil)
+values = Cons 1 (Cons 2 Nil)
+
+instance Applicative List where
+  pure a = Cons a Nil
+  (<*>) Nil _ = Nil
+  (<*>) _ Nil = Nil
+  (<*>) fs as = concat' $ fmap (\f -> fmap f as) fs
