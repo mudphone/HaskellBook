95375202f920d5d88d7ddce1a979c585d46db650
Kyle Oba <koba@mudphone.com>
2017-01-28 07:56:10 -1000

    Is this point free yet?

--- src/TwentyFive.hs
+++ src/TwentyFive.hs
@@ -54,40 +54,40 @@ class Bifunctor p where
 data Deux a b = Deux a b
 
 instance Bifunctor Deux where
-  bimap f g dab = second g (first f dab)
+  bimap f g = second g . first f
 
 -- 2
 data Const a b = Const a
 
 instance Bifunctor Const where
-  bimap f g ca = second g (first f ca)
+  bimap f g = second g . first f
 
 -- 3
 data Drei a b c = Drei a b c
 
 instance Bifunctor (Drei a) where
-  bimap f g dabc = second g (first f dabc)
+  bimap f g = second g . first f
 
 -- 4
 data SuperDrei a b c = SuperDrei a b
 
 instance Bifunctor (SuperDrei a) where
-  bimap f g sd = second g (first f sd)
+  bimap f g = second g . first f
 
 -- 5
 data SemiDrei a b c = SemiDrei a
 
 instance Bifunctor (SemiDrei a) where
-  bimap f g sd = second g (first f sd)
+  bimap f g = second g . first f
 
 -- 6
 data Quadriceps a b c d = Quadzzz a b c d
 
 instance Bifunctor (Quadriceps a b) where
-  bimap f g q = second g (first f q)
+  bimap f g = second g . first f 
 
 -- 7
 data Either' a b = Left a | Right b
 
 instance Bifunctor Either' where
-  bimap f g e = second g (first f e)
+  bimap f g = second g . first f
