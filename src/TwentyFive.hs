c5993ec8a51860644d52e94718d294c0e353c79b
Kyle Oba <koba@mudphone.com>
2017-01-28 07:52:47 -1000

    Add bifunctor exercises

--- src/TwentyFive.hs
+++ src/TwentyFive.hs
@@ -35,3 +35,59 @@ instance (Traversable f, Traversable g) =>
          Traversable (Compose f g) where
 --  traverse :: Applicative f => (a -> f b) -> t a -> f (t b)
   traverse f (Compose fga) = sequenceA (f <$> (Compose fga))
+
+
+-- And now for something completely different
+class Bifunctor p where
+  {-# MINIMAL bimap | first, second #-}
+
+  bimap :: (a -> b) -> (c -> d) -> p a c -> p b d
+  bimap f g = first f . second g
+
+  first :: (a -> b) -> p a c -> p b c
+  first f = bimap f id
+
+  second :: (b -> c) -> p a b -> p a c
+  second = bimap id
+
+-- 1
+data Deux a b = Deux a b
+
+instance Bifunctor Deux where
+  bimap f g dab = second g (first f dab)
+
+-- 2
+data Const a b = Const a
+
+instance Bifunctor Const where
+  bimap f g ca = second g (first f ca)
+
+-- 3
+data Drei a b c = Drei a b c
+
+instance Bifunctor (Drei a) where
+  bimap f g dabc = second g (first f dabc)
+
+-- 4
+data SuperDrei a b c = SuperDrei a b
+
+instance Bifunctor (SuperDrei a) where
+  bimap f g sd = second g (first f sd)
+
+-- 5
+data SemiDrei a b c = SemiDrei a
+
+instance Bifunctor (SemiDrei a) where
+  bimap f g sd = second g (first f sd)
+
+-- 6
+data Quadriceps a b c d = Quadzzz a b c d
+
+instance Bifunctor (Quadriceps a b) where
+  bimap f g q = second g (first f q)
+
+-- 7
+data Either' a b = Left a | Right b
+
+instance Bifunctor Either' where
+  bimap f g e = second g (first f e)
